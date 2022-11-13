0749f39b5077ad5b797af49bb730c8b6814dee23
Md Mohsin Siddiqui <mdmohsinsiddiqui5@gmail.com>
2022-11-13 22:31:09 +0530

    done

--- armstrong-numbers/src/lib.rs
+++ armstrong-numbers/src/lib.rs
@@ -1,3 +1,10 @@
 pub fn is_armstrong_number(num: u32) -> bool {
-    unimplemented!("true if {} is an armstrong number", num)
+    let digits :u32 = (num as f32).log10() as u32 + 1;
+    let mut temp = num;
+    let mut sum :u32 = 0;
+    while temp > 0 {
+        sum += (temp % 10).pow(digits);
+        temp /= 10;
+    }
+    sum == num
 }
