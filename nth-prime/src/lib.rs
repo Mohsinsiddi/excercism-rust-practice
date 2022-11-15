9a424c76fe6e863ada40933a42bb75cdbc08db10
Md Mohsin Siddiqui <mdmohsinsiddiqui5@gmail.com>
2022-11-15 12:50:30 +0530

    done

--- nth-prime/src/lib.rs
+++ nth-prime/src/lib.rs
@@ -1,3 +1,19 @@
+pub fn is_prime(n: u32) -> bool {
+    for i in 2..n {
+        if n % i == 0 {
+            return false
+        }
+    }
+    return true
+}
 pub fn nth(n: u32) -> u32 {
-    unimplemented!("What is the 0-indexed {}th prime number?", n)
+    let mut count = 0;
+    let mut number = 2;
+    while count != n {
+        number += 1;
+        if is_prime(number) {
+            count += 1
+        }
+    }
+    number
 }
