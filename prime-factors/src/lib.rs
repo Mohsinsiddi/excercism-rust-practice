db4b7b510b6722b70ee4fe512b4eb41b5a08f077
Md Mohsin Siddiqui <mdmohsinsiddiqui5@gmail.com>
2022-11-15 13:01:41 +0530

    done

--- prime-factors/src/lib.rs
+++ prime-factors/src/lib.rs
@@ -1,3 +1,15 @@
 pub fn factors(n: u64) -> Vec<u64> {
-    unimplemented!("This should calculate the prime factors of {}", n)
+    let mut v = Vec::new();
+    let mut rem = n;
+
+    for i in 2..=n{
+        while rem%i==0 {
+            v.push(i);
+            rem/=i;
+        }
+        if rem ==1 {
+            return v;
+        }
+    }
+    v
 }
