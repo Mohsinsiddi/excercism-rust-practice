aca32685b878e9bcb8493c47787aa8972ffbacc2
Md Mohsin Siddiqui <mdmohsinsiddiqui5@gmail.com>
2022-11-15 20:26:26 +0530

    done

--- matching-brackets/src/lib.rs
+++ matching-brackets/src/lib.rs
@@ -1,6 +1,13 @@
 pub fn brackets_are_balanced(string: &str) -> bool {
-    unimplemented!(
-        "Check if the string \"{}\" contains balanced brackets",
-        string
-    );
+    let mut char_stack = Vec::<char>::new();
+    for c in string.chars() {
+        match c {
+            '(' | '{' | '[' => char_stack.push(c),
+            ')' => if char_stack.pop()!=Some('(') {return  false},
+            ']' => if char_stack.pop()!=Some('[') {return  false},
+            '}' => if char_stack.pop()!=Some('{') {return  false},
+            _ =>()
+        }
+    }
+    char_stack.is_empty()
 }
