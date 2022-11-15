426842a59770e57d701a59285d011f2a8930ad0d
Md Mohsin Siddiqui <mdmohsinsiddiqui5@gmail.com>
2022-11-15 13:19:07 +0530

    done

--- sum-of-multiples/src/lib.rs
+++ sum-of-multiples/src/lib.rs
@@ -1,7 +1,15 @@
-pub fn sum_of_multiples(limit: u32, factors: &[u32]) -> u32 {
-    unimplemented!(
-        "Sum the multiples of all of {:?} which are less than {}",
-        factors,
-        limit
-    )
+use std::collections::BTreeSet;
+pub fn sum_of_multiples(limit: u32, numbers: &[u32]) -> u32 {
+    let mut multiples: BTreeSet<u32> = BTreeSet::new();
+    for &number in numbers {
+        let mut multiplier: u32 = 2;
+        let mut current = number;
+        if current == 0 {continue;}
+        while current < limit {
+            multiples.insert(current);
+            current = number * multiplier;
+            multiplier += 1;
+        }
+    }
+    multiples.iter().sum()
 }
