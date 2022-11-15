24487abbcac98a7029717966f848896d4cca5920
Md Mohsin Siddiqui <mdmohsinsiddiqui5@gmail.com>
2022-11-15 12:23:15 +0530

    done

--- grains/src/lib.rs
+++ grains/src/lib.rs
@@ -1,7 +1,16 @@
 pub fn square(s: u32) -> u64 {
-    unimplemented!("grains of rice on square {}", s);
+    match s {
+        0 => panic!("Square must be between 1 and 64"),
+        65..=u32::MAX => panic!("Square must be between 1 and 64"),
+        _ => square_method(s),
+    }
+   
+  
+}
+fn square_method(s:u32)->u64{
+    u64::pow(2, s-1)
 }
 
 pub fn total() -> u64 {
-    unimplemented!();
+   (1..=64).map(square_method).sum()
 }
