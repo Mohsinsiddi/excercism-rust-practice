ae5fa9eb6c103619b673062306c7ac7fc8a1573a
Md Mohsin Siddiqui <mdmohsinsiddiqui5@gmail.com>
2022-11-15 11:55:43 +0530

    done

--- beer-song/src/lib.rs
+++ beer-song/src/lib.rs
@@ -1,7 +1,17 @@
 pub fn verse(n: u32) -> String {
-    unimplemented!("emit verse {}", n)
+    match n {
+        3..=99 => format!("{} bottles of beer on the wall, {} bottles of beer.\nTake one down and pass it around, {} bottles of beer on the wall.\n", n, n, n-1),
+        2 => "2 bottles of beer on the wall, 2 bottles of beer.\nTake one down and pass it around, 1 bottle of beer on the wall.\n".to_string(),
+        1 => "1 bottle of beer on the wall, 1 bottle of beer.\nTake it down and pass it around, no more bottles of beer on the wall.\n".to_string(),
+        0 => "No more bottles of beer on the wall, no more bottles of beer.\nGo to the store and buy some more, 99 bottles of beer on the wall.\n".to_string(),
+        _ => String::new(),
+    }
 }
 
 pub fn sing(start: u32, end: u32) -> String {
-    unimplemented!("sing verses {} to {}, inclusive", start, end)
+    let mut output: Vec<String> = Vec::new();
+    for n in (end..=start).rev() {
+        output.push(verse(n));
+    }
+    output.join("\n")
 }
