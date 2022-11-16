20b18065735101dc373b81d2bf0ccf26b555bb4f
Md Mohsin Siddiqui <mdmohsinsiddiqui5@gmail.com>
2022-11-17 01:27:27 +0530

    done

--- collatz-conjecture/src/lib.rs
+++ collatz-conjecture/src/lib.rs
@@ -1,6 +1,23 @@
+use core::u64::MAX;
+
 pub fn collatz(n: u64) -> Option<u64> {
-    unimplemented!(
-        "return Some(x) where x is the number of steps required to reach 1 starting with {}",
-        n,
-    )
+    let mut x = n;
+    let mut steps = 0;
+    if x == 0 {
+        return None;
+    }
+    while x != 1 {
+        x = if x%2==0 {
+            x/2
+        } else {
+          match x.checked_mul(3) {
+            Some(y) if y < MAX.try_into().unwrap() => y + 1,
+            _ => return None,
+        }
+        };
+        steps+=1;
+
+    }
+    Some(steps)
+
 }
