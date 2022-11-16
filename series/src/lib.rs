086f6075d693132711b8bd5d00bc188c18a8ce49
Md Mohsin Siddiqui <mdmohsinsiddiqui5@gmail.com>
2022-11-17 01:05:24 +0530

    done

--- series/src/lib.rs
+++ series/src/lib.rs
@@ -1,7 +1,11 @@
 pub fn series(digits: &str, len: usize) -> Vec<String> {
-    unimplemented!(
-        "What are the series of length {} in string {:?}",
-        len,
-        digits
-    )
+    let mut result = Vec::new();
+    if digits.len() < len {
+        return result;
+    }
+    for i in 0..(digits.len()-len+1) {
+        let x = String::from(&digits[i..i+len]);
+        result.push(x);
+    }
+    result
 }
