1748d422ce66942b55fbfbc28d5b6b826878e3e4
Md Mohsin Siddiqui <mdmohsinsiddiqui5@gmail.com>
2022-11-17 11:54:13 +0530

    done

--- diffie-hellman/src/lib.rs
+++ diffie-hellman/src/lib.rs
@@ -1,21 +1,56 @@
+static mut STARTER: u64 = 2;
+
 pub fn private_key(p: u64) -> u64 {
-    unimplemented!("Pick a private key greater than 1 and less than {}", p)
+    prime_gen(p)
+}
+
+fn is_prime(n: u64) -> bool {
+    n == 2 || n % 2 > 0 && (3..=(n as f64).sqrt() as u64).step_by(2).all(|i| n % i > 0)
 }
 
 pub fn public_key(p: u64, g: u64, a: u64) -> u64 {
-    unimplemented!(
-        "Calculate public key using prime numbers {} and {}, and private key {}",
-        p,
-        g,
-        a
-    )
+    power_mod(g,a,p)
 }
 
 pub fn secret(p: u64, b_pub: u64, a: u64) -> u64 {
-    unimplemented!(
-        "Calculate secret key using prime number {}, public key {}, and private key {}",
-        p,
-        b_pub,
-        a
-    )
+   power_mod(b_pub, a, p)
+}
+
+fn prime_gen(end: u64) -> u64 {
+    let mut i;
+    unsafe {
+        i = STARTER;
+    }
+    loop {
+        
+        if i == end {
+            i = 2;
+        }
+        if is_prime(i) {
+            unsafe {
+                STARTER = i + 1;
+            }
+            return i;
+        }
+        i += 1;
+    }
+}
+
+fn power_mod(base:u64, a:u64, p: u64) -> u64 {
+    let mut ans:u64 = 1;
+    let mut base1:u64 = base;
+    let mut a1:u64 = a;
+    while a1 != 0 {
+        if a1 & 1 == 1 {
+            ans = calculate(ans, base1, p);
+        };
+        base1 = calculate(base1, base1, p);
+        a1 = a1 >> 1;
+    };
+    ans
+}
+
+fn calculate(a:u64, b:u64, p:u64) -> u64 {
+    let cl = (a as u128) * (b as u128) % (p as u128);
+    return cl as u64;
 }
