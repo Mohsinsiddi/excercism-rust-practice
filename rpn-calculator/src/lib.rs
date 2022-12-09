c62dcea2891a101954e2adf472d79e6ae91f4a79
Md Mohsin Siddiqui <mdmohsinsiddiqui5@gmail.com>
2022-12-09 12:44:11 +0530

    done

--- rpn-calculator/src/lib.rs
+++ rpn-calculator/src/lib.rs
@@ -6,10 +6,39 @@ pub enum CalculatorInput {
     Divide,
     Value(i32),
 }
-
 pub fn evaluate(inputs: &[CalculatorInput]) -> Option<i32> {
-    unimplemented!(
-		"Given the inputs: {:?}, evaluate them as though they were a Reverse Polish notation expression",
-		inputs,
-	);
+    let mut stack: Vec<i32> = Vec::new();
+    for input in inputs {
+        if match input {
+            CalculatorInput::Add => operator(&mut stack, |a, b| a + b),
+            CalculatorInput::Subtract => operator(&mut stack, |a, b| a - b),
+            CalculatorInput::Multiply => operator(&mut stack, |a, b| a * b),
+            CalculatorInput::Divide => operator(&mut stack, |a, b| a / b),
+            CalculatorInput::Value(n) => {
+                stack.push(*n);
+                Ok(())
+            }
+        }
+        .is_err()
+        {
+            return None;
+        }
+    }
+    if stack.len() != 1 {
+        None
+    } else {
+        stack.pop()
+    }
+}
+fn operator<F>(stack: &mut Vec<i32>, op: F) -> Result<(), ()>
+where
+    F: Fn(i32, i32) -> i32,
+{
+    if stack.len() < 2 {
+        return Err(());
+    }
+    let a = stack.pop().unwrap();
+    let b = stack.pop().unwrap();
+    stack.push(op(b, a));
+    Ok(())
 }
