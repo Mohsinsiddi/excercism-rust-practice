b77d63527772f4762873a03413f04013ea8fd15e
Md Mohsin Siddiqui <mdmohsinsiddiqui5@gmail.com>
2022-12-20 15:00:07 +0530

    test 4 passes

--- magazine-cutout/src/lib.rs
+++ magazine-cutout/src/lib.rs
@@ -5,5 +5,23 @@
 use std::collections::HashMap;
 
 pub fn can_construct_note(magazine: &[&str], note: &[&str]) -> bool {
-    unimplemented!()
+      let magazine_words = count_words(magazine);
+      let note_words = count_words(note);
+
+      for (word,word_count) in note_words {
+        let magazine_word_count = magazine_words.get(word).unwrap_or(&0);
+        if *magazine_word_count < word_count {
+            return  false;
+        }
+      }
+      return true;
+}
+
+fn count_words<'a>(text: &[& 'a str]) -> HashMap<&'a str,u32>{
+     let mut word_count = HashMap::new();
+     for word in text {
+        let mut count = word_count.entry(*word).or_insert(0);
+        *count+=1;
+     }
+     word_count
 }
