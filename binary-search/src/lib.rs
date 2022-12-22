0ebc4abb832635857f55c19a9374cf12c4b4926d
Md Mohsin Siddiqui <mdmohsinsiddiqui5@gmail.com>
2022-12-22 16:47:36 +0530

    rust done

--- binary-search/src/lib.rs
+++ binary-search/src/lib.rs
@@ -1,7 +1,16 @@
 pub fn find(array: &[i32], key: i32) -> Option<usize> {
-    unimplemented!(
-        "Using the binary search algorithm, find the element '{}' in the array '{:?}' and return its index.",
-        key,
-        array
-    );
+    let mut start = 0;
+    let mut end = array.len();
+    let mut mid = end / 2;
+    
+    while start != end {
+        use std::cmp::Ordering::*;
+        match key.cmp(&array[mid]) {
+            Less => end = mid,
+            Equal => return Some(mid),
+            Greater => start = mid + 1,
+        }
+        mid = start + (end - start) / 2;
+    }
+    None
 }
