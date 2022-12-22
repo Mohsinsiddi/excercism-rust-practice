45207a134186446b4d4be8fa0cc19730cb8c639a
Md Mohsin Siddiqui <mdmohsinsiddiqui5@gmail.com>
2022-12-22 16:49:05 +0530

    test 8 passed

--- binary-search/tests/binary-search.rs
+++ binary-search/tests/binary-search.rs
@@ -44,7 +44,6 @@ fn finds_a_value_in_an_array_of_odd_length() {
 }
 
 #[test]
-#[ignore]
 fn finds_a_value_in_an_array_of_even_length() {
     assert_eq!(
         find(&[1, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, 377], 21),
