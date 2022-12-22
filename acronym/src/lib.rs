f22be2c3dd44fb0e6d6dbeca6cdf62ea7e415b17
Md Mohsin Siddiqui <mdmohsinsiddiqui5@gmail.com>
2022-12-22 16:55:12 +0530

    done

--- acronym/src/lib.rs
+++ acronym/src/lib.rs
@@ -1,3 +1,14 @@
 pub fn abbreviate(phrase: &str) -> String {
-    unimplemented!("Given the phrase '{}', return its acronym", phrase);
+   let phrase = phrase.trim();
+
+   let phrase_words = phrase.split(" ").collect();
+
+   let mut ans = String::from("");
+
+   for words in  phrase_words {
+    let c = words.chars().collect::<Vec<char>>();
+       ans+=c[0]
+   }
+
+   ans
 }
