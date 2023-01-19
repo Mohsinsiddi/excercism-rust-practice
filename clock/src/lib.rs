cddbd053efbd71302686844d53e858df734dfc56
Md Mohsin Siddiqui <mdmohsinsiddiqui5@gmail.com>
2023-01-19 18:46:49 +0530

    test 1 done

--- clock/src/lib.rs
+++ clock/src/lib.rs
@@ -1,15 +1,27 @@
-pub struct Clock;
+use std::fmt;
+
+const DAY : i32 = 24 * 60;
+const HOUR : i32 = 60;
+
+#[derive(Debug,PartialEq)]
+pub struct Clock {
+    hours:i32,
+    minutes : i32
+}
 
 impl Clock {
-    pub fn new(hours: i32, minutes: i32) -> Self {
-        unimplemented!(
-            "Construct a new Clock from {} hours and {} minutes",
-            hours,
-            minutes
-        );
+    pub fn new(hours: i32, minutes: i32) -> Self {  
+        let total_minutes = (((hours * HOUR + minutes)%DAY)+DAY)%DAY;
+        Clock { hours: total_minutes/HOUR, minutes: total_minutes%HOUR }
     }
 
     pub fn add_minutes(&self, minutes: i32) -> Self {
-        unimplemented!("Add {} minutes to existing Clock time", minutes);
+       Clock { hours:self.hours, minutes: self.minutes+minutes }
+    }
+}
+
+impl fmt::Display for Clock {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f,"{:02}:{:02}",self.hours,self.minutes)
     }
 }
