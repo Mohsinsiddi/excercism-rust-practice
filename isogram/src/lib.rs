b8180150c6e593bca2d8087f9eddae0b74d48522
Md Mohsin Siddiqui <mdmohsinsiddiqui5@gmail.com>
2023-02-20 19:51:43 +0530

    done with rs

--- isogram/src/lib.rs
+++ isogram/src/lib.rs
@@ -1,3 +1,16 @@
+use std::collections::HashSet;
+
 pub fn check(candidate: &str) -> bool {
-    unimplemented!("Is {candidate} an isogram?");
+    let mut setMap = HashSet::new();
+
+    let lowercase_chars = candidate.to_lowercase();
+
+    let chars = lowercase_chars.chars().filter(|x| *x!=' ' && *x !='-');
+     
+    for c in chars {
+        if !setMap.insert(c) {
+            return  false;
+        }
+    }
+    return  true;
 }
